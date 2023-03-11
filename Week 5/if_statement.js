dcbe10bb873e2a3c5e384b973b9c45547ef79ba6
eculajay <97712003+eculajay@users.noreply.github.com>
2023-03-10 20:20:43 -0600

    Create if_statement.js

---
+++ Week 5/if_statement.js
@@ -0,0 +1,14 @@
+var a = 81;
+if (a > 79) {
+    console.log("Congratulations! you have a passing score: ",a)
+}
+else{
+    console.log("Never give up! your socre is: ",a)
+}
+// CoreCode EC 
+
+var a = 0;
+while (a < 7) {
+    console.log("The number is " + i);
+    a++;
+  }

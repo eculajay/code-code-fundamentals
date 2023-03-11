72bf4b8d41251d9b85198cc12bb688ce72762d61
eculajay <97712003+eculajay@users.noreply.github.com>
2023-03-11 01:37:07 -0600

    Add files via upload

---
+++ Week 6/Week_challenges(Tuesday).js
@@ -0,0 +1,31 @@
+// 2. Variables
+let firstname = 'Lata';
+console.log(firstname);
+
+
+// 3. What is x?
+let x = 'Geeta';
+console.log*('The solution is "Geeta') // Solution = "Geeta"
+
+
+// 4. Several variables
+let flower = 'rose';
+let tree = 'maple';
+console.log(flower);
+console.log(tree);
+
+
+// 5. Reassigment
+let x = 'Tic';
+x = 'Tac';
+x = 'Toe';
+console.log('Toe is on x'); // Solition = 'Toe'
+
+
+// 6. Assign variables
+let x = 'Laurel';
+let y = 'Hardy';
+let z = y;
+y = x;
+x = z;
+console.log('"Hardy" is on x'); // Solution = 'Hardy'

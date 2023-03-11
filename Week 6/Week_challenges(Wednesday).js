6fe1db262f5533236ff5ffd600ecd898f08ea467
eculajay <97712003+eculajay@users.noreply.github.com>
2023-03-11 01:37:28 -0600

    Add files via upload

---
+++ Week 6/Week_challenges(Wednesday).js
@@ -0,0 +1,41 @@
+// 2. Functions
+function hello() {; 
+    return "Hello world!"
+ }
+
+
+// 3. Multiple functions 
+function a() {
+    return "Hello a!";
+ }
+    function b() {
+    return "Hello b!";
+ }
+    console.log(a);
+    console.log(b); 
+
+
+// 4. Function calls
+let salutation = greet ();
+    function greet () {
+    return 'Haydo!' ;
+} 
+
+// 5. What is x? (function version)
+function reply(phrase) {
+  return phrase;
+}
+let x = reply('How do you do?');
+Solution 
+/* Solution: x = "How do you do?" */
+
+
+// 6. Parameters
+function echo(message) {
+    return message;
+}
+let resultecho = echo("Greta");
+console.log(resultecho)
+
+
+

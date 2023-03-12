ca0d645501f689dc05abf0a34a619f84fb974b52
eculajay <97712003+eculajay@users.noreply.github.com>
2023-03-12 00:25:33 -0600

    Update Week7_challenges(Monday).js

--- Week 7/Week7_challenges(Monday).js
+++ Week 7/Week7_challenges(Monday).js
@@ -1,6 +1,14 @@
 // 1. String: substr()
   function firstWord (){
-  let str1 = 'See and stop';
-  return str1.substring(0,3);
+    let str1 = 'See and stop';
+    return str1.substring(0,3);
   }
   console.log(firstWord()); 
+
+// 2. String: replace()
+  function normalize() {
+    let dates = "20 - 05 - 2017";
+    return dates.replace(/-/g, "/");
+
+  }
+  console.log(normalize())

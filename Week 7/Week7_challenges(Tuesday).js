d5b3ba38976a6c397c1dd6a850a25fe35f193e81
eculajay <97712003+eculajay@users.noreply.github.com>
2023-03-14 23:56:42 -0600

    Update Week7_challenges(Tuesday).js

--- Week 7/Week7_challenges(Tuesday).js
+++ Week 7/Week7_challenges(Tuesday).js
@@ -5,3 +5,10 @@ function animal(obj){
   animal.color='White';
   return 'This ' + obj.color + ' ' + obj.name + ' has ' + obj.legs + ' legs.';
 }
+
+// 2. Mistery
+function mystery() {
+  var results =
+    {sanity: 'Hello'};
+  return results;
+}

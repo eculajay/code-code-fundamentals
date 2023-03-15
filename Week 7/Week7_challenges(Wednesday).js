afad022528df728c2200fb987c8fd4bebd9b9407
eculajay <97712003+eculajay@users.noreply.github.com>
2023-03-15 00:22:30 -0600

    Create Week7_challenges(Wednesday).js

---
+++ Week 7/Week7_challenges(Wednesday).js
@@ -0,0 +1,10 @@
+// 1. Count
+function strCount(obj){
+  let count = 0
+  for (key in obj){
+    if (typeof obj[key] == 'string') count++;
+    if (typeof obj[key] == 'object') count+= strCount(obj[key]);
+  }
+  
+  return count
+}

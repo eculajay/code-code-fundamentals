78429f67f35d73e1f1a3bc6564be69c2b2f70c24
eculajay <97712003+eculajay@users.noreply.github.com>
2023-03-14 22:31:15 -0600

    Update Week7_challenges(Monday).js

--- Week 7/Week7_challenges(Monday).js
+++ Week 7/Week7_challenges(Monday).js
@@ -7,8 +7,29 @@
 
 // 2. String: replace()
   function normalize() {
-    let dates = "20 - 05 - 2017";
-    return dates.replace(/-/g, "/");
+    let dates = '20 - 05 - 2017';
+    return dates.replace(/-/g, '/');
 
   }
   console.log(normalize());
+
+// 3. Increment 
+  let x = 3;
+  x++;
+  x = x * 2;
+  x--;
+  console.log(x)
+
+// 4. Fahrenheit 
+  function toFahrenheit(celsius) {
+      return 1.8 * celsius + 32;
+  }
+  console.log(toFahrenheit(0))
+
+
+// 5. Boolean 
+  function nand(y, z) {
+      let and = y && z;
+      return !and;
+  }
+  console.log(nand())

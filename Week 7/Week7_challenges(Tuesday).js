9fcd61fa1f67453e5f7bbc4c17b637ba879bce11
eculajay <97712003+eculajay@users.noreply.github.com>
2023-03-14 23:53:14 -0600

    Create Week7_challenges(Tuesday).js

---
+++ Week 7/Week7_challenges(Tuesday).js
@@ -0,0 +1,7 @@
+// 1. Objects
+function animal(obj){
+  animal.name='dog';
+  animal.legs='4';
+  animal.color='White';
+  return 'This ' + obj.color + ' ' + obj.name + ' has ' + obj.legs + ' legs.';
+}
